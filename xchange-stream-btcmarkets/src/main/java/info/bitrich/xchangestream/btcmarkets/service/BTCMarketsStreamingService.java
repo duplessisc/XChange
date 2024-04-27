d9c585e412b07ffc4584031e0abe4adfc2865add
Christiaan <duplessiscdw@gmailcom>
2024-04-27 19:16:40 +1000

    Merge updates from main branch

--- xchange-stream-btcmarkets/src/main/java/info/bitrich/xchangestream/btcmarkets/service/BTCMarketsStreamingService.java
+++ xchange-stream-btcmarkets/src/main/java/info/bitrich/xchangestream/btcmarkets/service/BTCMarketsStreamingService.java
@@ -19,8 +19,19 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BTCMarketsStreamingService extends JsonNettyStreamingService {
+import com.fasterxml.jackson.databind.JsonNode;
+import info.bitrich.xchangestream.btcmarkets.dto.BTCMarketsWebSocketSubscriptionMessage;
+import info.bitrich.xchangestream.service.netty.JsonNettyStreamingService;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class BTCMarketsStreamingService extends JsonNettyStreamingService {
   //	TODO change to enumerator
   static final String CHANNEL_ORDERBOOK = "orderbook";
   static final String CHANNEL_HEARTBEAT = "heartbeat";
@@ -28,22 +39,11 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
   static final String CHANNEL_TRADE = "trade";
 
   private static final Logger LOG = LoggerFactory.getLogger(BTCMarketsStreamingService.class);
-  /** public channels are channels that are available publicly and do not need authentication */
-  private final List<String> publicChannels =
-      Lists.newArrayList(CHANNEL_ORDERBOOK, CHANNEL_HEARTBEAT, CHANNEL_TICKER, CHANNEL_TRADE);
-  /** private channels are channels that requires and authenticated/signed message */
-  private final List<String> privateChannels = Lists.newArrayList();
-
-  private final ObjectMapper mapper = StreamingObjectMapperHelper.getObjectMapper();
-
   private final ConcurrentHashMap<String, Set<String>> subscribedMarketIds =
       new ConcurrentHashMap<String, Set<String>>();
-  private Observable<JsonNode> heartbeat;
-  private String heartbeatChannel;
 
   public BTCMarketsStreamingService(String apiUrl) {
     super(apiUrl);
-    mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
   }
 
   /*
@@ -57,14 +57,14 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
     // Create the first subscription message
     if (!hasActiveSubscriptions()) {
       return BTCMarketsWebSocketSubscriptionMessage.getFirstSubscriptionMessage(
-          marketIds == null ? null : Lists.newArrayList(marketIds),
-          Lists.newArrayList(channelName, CHANNEL_HEARTBEAT),
+          new ArrayList<String>(marketIds),
+          Arrays.asList(channelName, CHANNEL_HEARTBEAT),
           null,
           null,
           null);
     } else {
       return BTCMarketsWebSocketSubscriptionMessage.getAddSubscriptionMessage(
-          Lists.newArrayList(marketIds), Lists.newArrayList(channelName), null, null, null);
+          new ArrayList<String>(marketIds), Arrays.asList(channelName), null, null, null);
     }
   }
 
@@ -72,8 +72,8 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
       String channelName, Set<String> marketIds) {
 
     return BTCMarketsWebSocketSubscriptionMessage.getRemoveSubcriptionMessage(
-        marketIds == null ? new ArrayList<String>() : Lists.newArrayList(marketIds),
-        Lists.newArrayList(channelName),
+        marketIds == null ? new ArrayList<String>() : new ArrayList<String>(marketIds),
+        Arrays.asList(channelName),
         null,
         null,
         null);
@@ -81,19 +81,21 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
 
   @Override
   protected String getChannelNameFromMessage(JsonNode message) {
-    LOG.trace("entering: getChannelNameFromMessage");
     final String messageType = message.get("messageType").asText();
-
-    if (messageType.startsWith(CHANNEL_HEARTBEAT)) return heartbeatChannel;
-    if (message.get("marketId") != null)
+    if (messageType.startsWith(CHANNEL_ORDERBOOK)
+        || messageType.startsWith(CHANNEL_TICKER)
+        || messageType.startsWith(CHANNEL_TRADE)) {
       return messageType + ":" + message.get("marketId").asText();
-    else return messageType;
+    }
+    return messageType;
   }
 
   @Override
   public String getSubscribeMessage(String channelName, Object... args) throws IOException {
-    LOG.trace("entering: getSubscribeMessage");
-    if (publicChannels.contains(channelName)) {
+
+    if (CHANNEL_ORDERBOOK.equals(channelName)
+        || CHANNEL_TICKER.equals(channelName)
+        || CHANNEL_TRADE.equals(channelName)) {
 
       LOG.debug("Now subscribing to {}:{}", channelName, args);
       Set<String> newMarketIds = new HashSet<String>();
@@ -115,13 +117,7 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
           channelName,
           subscribedMarketIds.get(channelName),
           newMarketIds);
-
-      BTCMarketsWebSocketSubscriptionMessage subscriptionMsg =
-          buildSubscribeMessage(channelName, newMarketIds);
-      if (subscriptionMsg.channels.contains(CHANNEL_HEARTBEAT))
-        heartbeatChannel = getSubscriptionUniqueId(channelName, args);
-      return objectMapper.writeValueAsString(subscriptionMsg);
-
+      return objectMapper.writeValueAsString(buildSubscribeMessage(channelName, newMarketIds));
     } else {
       throw new IllegalArgumentException(
           "Can't create subscribe messsage for channel " + channelName);
@@ -130,17 +126,15 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
 
   @Override
   public String getSubscriptionUniqueId(String channelName, Object... args) {
-
-    String uniqueId = args == null ? channelName : channelName + ":" + args[0].toString();
-
-    LOG.debug("Returning unique id {}", uniqueId);
-    return uniqueId;
+    LOG.debug("Returning unique id {}", channelName + ":" + args[0].toString());
+    return channelName + ":" + args[0].toString();
   }
 
   @Override
-  public String getUnsubscribeMessage(String channelName) throws IOException {
-
-    if (publicChannels.contains(channelName) | publicChannels.contains(channelName.split(":")[0])) {
+  public String getUnsubscribeMessage(String channelName, Object... args) throws IOException {
+    if (channelName.startsWith(CHANNEL_ORDERBOOK)
+        || channelName.startsWith(CHANNEL_TICKER)
+        || channelName.startsWith(CHANNEL_TRADE)) {
       LOG.debug(
           "getUnsubscribeMessage: what is in subscribedMarketIds {}:{}",
           channelName,
@@ -153,35 +147,7 @@ public class BTCMarketsStreamingService extends JsonNettyStreamingService {
   }
 
   private Boolean hasActiveSubscriptions() {
-    /*
-     * TODO - hasActiveSubscription: this effectively looks at an internally managed list of channels and
-     * their associated subscriptions this whole concept should be enhanced to handle the heartbeat event and update the channels
-     * accordingly (heartbeat is produced every time a channel is subscribed). One potential implementation is to always first
-     * subscribe to heartbeat and blockwait for this to return before adding any other subscriptions.
-     */
     return !channels.isEmpty();
   }
-
-  @Override
-  public Observable<JsonNode> subscribeChannel(String channelName, Object... args) {
-
-    boolean hasActiveSubription = hasActiveSubscriptions();
-    Observable<JsonNode> subscription = super.subscribeChannel(channelName, args);
-    // process hearbeat events
-    if (!hasActiveSubription) {
-      heartbeat = subscription;
-      heartbeat
-          .filter(node -> node.findValue("messageType").asText().equals(CHANNEL_HEARTBEAT))
-          .map(node -> mapper.treeToValue(node, BTCMarketsWebSocketHeartbeatMessage.class))
-          .forEach(hearbeat -> LOG.info("heartbeat -  {}", hearbeat));
-    }
-
-    return subscription;
-  }
-
-  /** @return {@code} Observable<JsonNode> that will receive heartbeat events} */
-  public Observable<JsonNode> getHeartbeatSubscription() throws NonReadableChannelException {
-    if (heartbeat == null) throw new NonReadableChannelException();
-    return heartbeat;
-  }
 }
+
